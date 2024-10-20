ee98e0c4d1ef2360a5606d1f8107b5235b786978
sasanpy <sasan.sp92@hotmail.com>
2024-10-20 20:57:29 +0330

    Correct project3_use_pymo(multi_object).py!

--- Genetaic Algorithm Course/project3_use_pymo(multi_boject).py
+++ Genetaic Algorithm Course/project3_use_pymo(multi_boject).py
@@ -31,23 +31,22 @@ from pymoo.visualization.scatter import Scatter
 class MyProblem(ElementwiseProblem):
 
     def __init__(self):
-
-        super().__init__(n_var = 3,
-                         n_obj = 2,
-                         n_ieq_constr = 2,
-                         xl = np.array([-10, -10, -10]),
-                         xu = np.array([10, 10, 10]))
+        super().__init__(n_var=3,
+                         n_obj=2,
+                         n_ieq_constr=2,
+                         xl=np.array([-10, -10, -10]),
+                         xu=np.array([10, 10, 10]))
 
     def _evaluate(self, x, out, *args, **kwargs):
-
-        f1 = x[0]**2 + x[1]**2 + x[2]**2
-        f2 = (x[0]-1)**2 + (x[1]-4)**2 + (x[2]-3)**2
+        f1 = x[0] ** 2 + x[1] ** 2 + x[2] ** 2
+        f2 = (x[0] - 1) ** 2 + (x[1] - 4) ** 2 + (x[2] - 3) ** 2
 
         g1 = x[0] + x[1] - x[2] - 1
-        g2 = -3 * x[0] + x[1] + x[2] - 4
+        g2 = -3 * x[0] + x[1] + x[2] + 4
 
         out["F"] = [f1, f2]
         out["G"] = [g1, g2]
+
 
 problem = MyProblem()
 
@@ -55,12 +54,12 @@ problem = MyProblem()
 Config.warnings['not_compiled'] = False
 
 algorithm = NSGA2(
-    pop_size = 50,
-    n_offsprings = 10,
-    sampling = FloatRandomSampling(),
-    crossover = SBX(prob = 0.9, eta = 20),
-    mutation = PM(eta = 25),
-    eliminate_duplicates = True,
+    pop_size=50,
+    n_offsprings=10,
+    sampling=FloatRandomSampling(),
+    crossover=SBX(prob=0.9, eta=20),
+    mutation=PM(eta=25),
+    eliminate_duplicates=True,
 
 )
 
@@ -70,7 +69,7 @@ termination = get_termination("n_gen", 100)
 res = minimize(problem,
                algorithm,
                termination,
-               seed = 7,
+               seed=7,
                save_history=True,
                verbose=True,
                )
@@ -82,7 +81,14 @@ print("\n---------\n")
 F = res.F
 print(F)
 
-plot = Scatter(title = "Three Variables Solutions")
+plot = Scatter(title="Three Variables Solutions")
 plot.add(F, color="red")
 plot.show()
+
+ideal_point = F.min(axis=0)
+nadir_point = F.max(axis=0)
+
+print("ideal point: ", ideal_point)
+print("nadir point: ", nadir_point)
+
 
